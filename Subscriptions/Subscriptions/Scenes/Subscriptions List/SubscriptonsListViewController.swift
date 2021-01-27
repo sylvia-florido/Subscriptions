b156b4dc9530fc0e27d29456cffa6cd13a3ae943
smara <smara128@gmail.com>
2021-01-27 18:12:32 -0300

    Add a Search feature to the subscriptions list.

--- Subscriptions/Subscriptions/Scenes/Subscriptions List/SubscriptonsListViewController.swift
+++ Subscriptions/Subscriptions/Scenes/Subscriptions List/SubscriptonsListViewController.swift
@@ -16,20 +16,40 @@ class SubscriptonsListViewController: BaseViewController {
     var repository: SubscriptionsRepository?
     var subscriptions: [SubscriptionDetails] = [] {
         didSet {
+            filteredResults = subscriptions
+        }
+    }
+    var filteredResults: [SubscriptionDetails] = [] {
+        didSet{
             tableView.reloadData()
         }
     }
     
-    let leftBarButton: UIBarButtonItem = {
-        let button = UIBarButtonItem(title: SubscriptionsListStrings.leftButtonTitle, style: .plain, target: self, action: #selector(leftBarButtonPressed))
+    lazy var leftBarButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(title: SubscriptionsListStrings.leftButtonTitle, style: .plain, target: self, action: #selector(leftBarButtonPressed(_:)))
         button.setTitleTextAttributes([NSAttributedString.Key.font:  UIFont(name: "TTCommons-Medium", size: 20) ?? UIFont.systemFont(ofSize: 16, weight: .semibold)], for: UIControl.State.normal)
         return button
     }()
-    
-    let rightBarButton: UIBarButtonItem = {
-        let button = UIBarButtonItem(image: UIImage(named: "icon_search"), style: .plain, target: self, action: #selector(rightBarButtonPressed))
+
+    lazy var rightBarButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(image: UIImage(named: "icon_search"), style: .plain, target: self, action: #selector(rightBarButtonPressed(_:)))
         button.tintColor = Colors.mediumGray
         return button
+    }()
+    
+    lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.sizeToFit()
+        definesPresentationContext = true
+        searchController.searchBar.barStyle = .blackTranslucent
+        searchController.searchBar.tintColor = Colors.mediumGray
+        navigationItem.searchController = searchController
+        searchController.hidesNavigationBarDuringPresentation = true
+
+        return searchController
     }()
     
     // MARK: - Lifecycle & Setup
@@ -44,6 +64,7 @@ class SubscriptonsListViewController: BaseViewController {
         super.configureNavigationBar()
         navigationItem.leftBarButtonItem = leftBarButton
         navigationItem.rightBarButtonItem = rightBarButton
+        navigationItem.hidesSearchBarWhenScrolling = false
     }
     
     private func setupTableView() {
@@ -74,26 +95,49 @@ class SubscriptonsListViewController: BaseViewController {
     
     // MARK: - Actions
     @objc func leftBarButtonPressed(_ sender: Any?) {
+        // Ofertas
+        print("OFertas")
     }
     
     @objc func rightBarButtonPressed(_ sender: Any?) {
+        // Search
+        print("Seacrh")
+        
+        showSearchBar(searchBar: searchController.searchBar)
     }
     
+    func showSearchBar(searchBar : UISearchBar) {
+        navigationItem.searchController = searchController
+        searchBar.becomeFirstResponder()
+        UIView.animate(withDuration: 0.5, animations: {
+            searchBar.alpha = 1
+        }, completion: { finished in
+        })
+    }
+
+    func hideSearchBar( searchBar : UISearchBar) {
+        searchBar.resignFirstResponder()
+        UIView.animate(withDuration: 0.3, animations: {
+            searchBar.alpha = 0
+        }, completion: { finished in
+            self.navigationItem.searchController = nil
+        })
+     }
 }
 
 extension SubscriptonsListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        subscriptions.count
+        filteredResults.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let model = subscriptions[indexPath.row]
+        let model = filteredResults[indexPath.row]
 
         let viewModel = SubscriptionsListViewModel(with: model)
         let cell = tableView.dequeueCell(SubscriptionListTableViewCell.self, indexPath: indexPath)
         cell.configure(with: viewModel)
         cell.delegate = self
-        let image = fetchImageForSideBackground(for: model) { (image) in
+        fetchImageForSideBackground(for: model) { (image) in
             cell.leftImageView?.image = image
         }
         return cell
@@ -107,6 +151,28 @@ extension SubscriptonsListViewController: SubscriptionListTableViewCellDelegate 
             return
         }
         let subscriptionSelected = subscriptions[index]
-        coordinator?.showDetailsScene(with: subscriptionSelected)
+        coordinator?.showDetailsScene(with: subscriptionSelected, subscriptionsList: subscriptions)
+    }
+}
+
+extension SubscriptonsListViewController: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let text = searchController.searchBar.text, !text.isEmpty {
+            filterContentForSearchText(text)
+        } else {
+            filteredResults = subscriptions
+        }
+    }
+    
+    func filterContentForSearchText(_ searchText: String) {
+        filteredResults = subscriptions.filter { (viewModel) -> Bool in
+            return viewModel.name.lowercased().contains(searchText.lowercased())
+        }
+//        listView.viewModel = filteredResults
+    }
+    
+    //MARK: UISearchBarDelegate
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        hideSearchBar(searchBar: searchBar)
     }
 }
